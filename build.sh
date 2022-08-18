cc3858ec3763b46014b5d615d091a309bf5e5884
dennisppaul <dennis.paul@hfk-bremen.de>
2022-08-18 18:02:20 +0200

    updated release

--- build.sh
+++ build.sh
@@ -1,11 +1,32 @@
 #! /bin/zsh
 
-build_archive() {
-	M_EXTENSION=tar.bz2
-    tar -cpjSf $1.$M_EXTENSION $1
-    ls -l $1.$M_EXTENSION
-    sha256sum $1.$M_EXTENSION
+PATH_TO_ARCHIVES=../
+
+print_line() {
+    echo "--------------------------------------------------------------------------------------------------------"
 }
 
-build_archive klangstrom-arduino-stm32-0.0.3
-build_archive klangstrom-arduino-desktop-0.0.3
+build_archive() {
+    M_PACKAGE_NAME=$1
+    M_KLST_LIB_PATH=klangstrom/klangstrom/platforms/klangstrom-arduino
+    M_VERSION=$2
+    M_EXTENSION=tar.bz2
+    M_ARCHIVE_NAME=klangstrom-arduino-$M_PACKAGE_NAME-$M_VERSION
+
+    echo
+    print_line
+    echo $M_ARCHIVE_NAME
+    print_line
+#     tar -cpjSf $M_ARCHIVE_NAME.$M_EXTENSION -C $M_KLST_LIB_PATH/$M_PACKAGE_NAME .
+    cp $PATH_TO_ARCHIVES/$M_ARCHIVE_NAME.$M_EXTENSION .
+    echo -n "checksum: SHA-256:"
+    sha256sum $M_ARCHIVE_NAME.$M_EXTENSION | awk '{print $1}'
+    echo -n "size: "
+    ls -l $M_ARCHIVE_NAME.$M_EXTENSION | awk '{print $5}'
+
+    print_line
+    echo
+}
+
+build_archive desktop 0.0.3
+build_archive stm32 0.0.3
