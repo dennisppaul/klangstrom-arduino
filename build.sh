3872ba3c261cde37de1742d161e682d0aec6473c
dennisppaul <dennis.paul@hfk-bremen.de>
2022-08-19 23:52:19 +0200

    updated script to automatically replace SIZE and CHECKSUM in json

--- build.sh
+++ build.sh
@@ -11,18 +11,25 @@ build_archive() {
     M_KLST_LIB_PATH=klangstrom/klangstrom/platforms/klangstrom-arduino
     M_VERSION=$2
     M_EXTENSION=tar.bz2
-    M_ARCHIVE_NAME=klangstrom-arduino-$M_PACKAGE_NAME-$M_VERSION
+    M_PROJECT_PREFIX=klangstrom-arduino
+    M_ARCHIVE_NAME=$M_PROJECT_PREFIX-$M_PACKAGE_NAME-$M_VERSION
+    M_JSON_SUFFIX=_index.json
+    M_JSON_NAME=package_$M_PROJECT_PREFIX-$M_PACKAGE_NAME$M_JSON_SUFFIX
 
     echo
     print_line
     echo $M_ARCHIVE_NAME
     print_line
 #     tar -cpjSf $M_ARCHIVE_NAME.$M_EXTENSION -C $M_KLST_LIB_PATH/$M_PACKAGE_NAME .
-    cp $PATH_TO_ARCHIVES/$M_ARCHIVE_NAME.$M_EXTENSION .
-    echo -n "checksum: SHA-256:"
-    sha256sum $M_ARCHIVE_NAME.$M_EXTENSION | awk '{print $1}'
-    echo -n "size: "
-    ls -l $M_ARCHIVE_NAME.$M_EXTENSION | awk '{print $5}'
+     cp $PATH_TO_ARCHIVES/$M_ARCHIVE_NAME.$M_EXTENSION .
+
+    CHECKSUM=$(sha256sum $M_ARCHIVE_NAME.$M_EXTENSION | awk '{print $1}')
+    echo "checksum: SHA-256:" $CHECKSUM
+
+    SIZE=$(ls -l $M_ARCHIVE_NAME.$M_EXTENSION | awk '{print $5}')
+    echo "size:" $SIZE
+
+    cat $M_JSON_NAME.template | sed -E "s/\{\{SIZE\}\}/${SIZE}/;s/\{\{CHECKSUM\}\}/${CHECKSUM}/"\ > $M_JSON_NAME
 
     print_line
     echo
